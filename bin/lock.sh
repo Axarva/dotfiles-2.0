0d1a9765ce25003dab45ba26b799d69dd9e785b6
Axarva <timsinaatharva@gmail.com>
2021-01-12 21:37:48 +0545

    Fix shebang lines

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 xset s 240 &
 xautolock -time 2 -locker "betterlockscreen -l" -notify 30 -notifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "loginctl suspend"
 #xautolock -time 2 -locker "systemctl suspend"
