f687717b934ec8949ece848d0d5b48e40805e84c
Axarva <timsinaatharva@gmail.com>
2021-01-17 11:50:05 +0545

    Make autolock reasonable

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-xset s 240 &
-xautolock -time 2 -locker "betterlockscreen -l" -notify 30 -notifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "loginctl suspend"
+xset s 500 &
+xautolock -time 5 -locker "betterlockscreen -l" -notify 30 -nodtifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "loginctl suspend"
 #xautolock -time 2 -locker "systemctl suspend"
