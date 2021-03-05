9b72ed68881e9e001fff2b63eeb8e3e6a4905827
Axarva <timsinaatharva@gmail.com>
2021-03-05 21:41:24 +0545

    Add images for network script

--- config/tint2/scripts/networkclick.sh
+++ config/tint2/scripts/networkclick.sh
@@ -4,12 +4,12 @@ x=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
 y=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
 
 if [ -z "$x" ] && [ -z "$y" ]; then
-    notify-send "Not Connected"
+    notify-send "Not Connected" -i ~/.config/dunst/images/no-connection.png
     exit 1
 elif [ -z "$x" ]; then 
-    notify-send "Connected to $y"
+    notify-send "Connected to $y" -i ~/.config/dunst/images/wifi.png
     exit 1
 elif [ -z "$y" ]; then
-    notify-send "Connected to $x"
+    notify-send "Connected to $x" -i ~/.config/dunst/images/ethernet.png
     exit 1
 fi
