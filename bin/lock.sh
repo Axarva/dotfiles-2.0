ec5a4c3bd5be3c1ebeb0f29212c09903414ca65d
kellelap <kellegram@protonmail.ch>
2021-05-19 02:55:09 +0100

    Bashify scripts for compatibility

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 xset s 500 &
 xautolock -time 5 -locker "betterlockscreen -l" -notify 30 -nodtifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "loginctl suspend"
-#xautolock -time 2 -locker "systemctl suspend"
