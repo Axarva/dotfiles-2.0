1e1fc6a33fd2dccc426cc9fb3b839964831b1fde
kellelap <kellegram@protonmail.ch>
2021-05-22 02:37:16 +0100

    Replace doas and loginctl
    Since this is a public setup, it would be sensible to have saner defaults.

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xset s 500 &
-xautolock -time 5 -locker "betterlockscreen -l" -notify 30 -nodtifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "loginctl suspend"
+xautolock -time 5 -locker "betterlockscreen -l" -notify 30 -nodtifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "systemctl suspend"
