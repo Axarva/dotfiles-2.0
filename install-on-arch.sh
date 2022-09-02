7be5651855a1bdeb2cd905e529fd60a6f72f269c
Axarva <66304313+Axarva@users.noreply.github.com>
2022-09-02 16:53:40 +0545

    Update install-on-arch.sh

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -83,7 +83,10 @@ $HELPER -S picom-jonaburg-git\
 	   xautolock         \
 	   betterlockscreen
 
-sudo ln -s /usr/lib/libasan.so.8 /usr/lib/libasan.so.6 #Hack for tint2 to work.
+# Hack to make tint2 work for now.
+if ! [ -f /usr/lib/libasan.so.6 ]; then
+    sudo ln -s /usr/lib/libasan.so.8 /usr/lib/libasan.so.6 # Hack to make tint2 work for now.
+fi
 
 #install custom picom config file 
 mkdir -p ~/.config/
